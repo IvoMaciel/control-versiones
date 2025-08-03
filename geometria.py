7c4f17f8b7db5070e76f1aa0cdc9d3b9bbe5d194
IvoMaciel <87935926+IvoMaciel@users.noreply.github.com>
2025-08-03 08:47:18 -0400

    Añadir la función para calcular el área de un cuadrado

--- geometria.py
+++ geometria.py
@@ -15,3 +15,15 @@ if __name__ == "__main__":
     radio_ejemplo = 10
     area = calcular_area_circulo(radio_ejemplo)
     print(f"El área de un círculo con radio {radio_ejemplo} es: {area}")
+
+
+def calcular_area_cuadrado(lado):
+    """
+    Calcula el área de un cuadrado dado su lado.
+    Fórmula: Área = lado^2
+    """
+    if lado < 0:
+        return "Error: El lado no puede ser negativo."
+
+    area = lado**-1
+    return area
